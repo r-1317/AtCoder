e1f1661ed4c3521b0552df9c8aeeb5b5cd62878a
r1317 <122949375+r-1317@users.noreply.github.com>
2024-06-16 18:23:24 +0900

    ABC358-Dまで

--- 2024/ABC358/d.py
+++ 2024/ABC358/d.py
@@ -11,7 +11,30 @@ else:
 ic.enable() if MyPC else None
 
 def main():
-  pass
+  n, m = map(int, input().split())
+  a_list = list(map(int, input().split()))
+  b_list = list(map(int, input().split()))
+
+  a_list.sort()
+  b_list.sort()
+
+  ic(a_list)
+  ic(b_list)
+
+  ans = 0
+  tmp = 0
+
+  for i in range(n):
+    if b_list[tmp] <= a_list[i]:
+      tmp += 1
+      ans += a_list[i]
+      if tmp == m:
+        break
+
+  if tmp == m:
+    print(ans)
+  else:
+    print(-1)
 
 if __name__ == "__main__":
   main()
