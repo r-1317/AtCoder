51ab0c661c2b1c8c81b5b8be157fb1ec523f1f04
r1317 <122949375+r-1317@users.noreply.github.com>
2024-07-31 18:03:45 +0900

    AHC035_更新3

--- 2024/AHC035/a01.py
+++ 2024/AHC035/a01.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 
 MyPC = os.path.basename(__file__) != "Main.py"
 if MyPC:
@@ -9,6 +10,9 @@ else:
     return None
 
 ic.enable() if MyPC else None
+
+def cos_sim(v1, v2):
+  return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
 
 #  評価項目の平均以下が少ない順にソート
 def sort01(seed_list):
@@ -115,6 +119,55 @@ def sort04(seed_list):
 
   return plant_list
 
+# cosine類似度を使用してソート
+def sort05(seed_list):
+  avg_list = [0]*m  # 種の評価項目の平均リスト
+
+  for i in range(m):
+    avg_list[i] = sum([seed_list[j][i] for j in range(60)]) / 60  # 評価項目の平均値を計算
+
+  ic(avg_list)
+
+  score_list = [[i, 0] for i in range(60)]  # 種の評価項目の平均との差の2乗リスト
+
+  for i in range(60):
+    for j in range(m):
+      score_list[i][1] += max((seed_list[i][j] - avg_list[j]),0) ** 2
+
+  ic(score_list)
+
+  score_list.sort(key=lambda x: x[1], reverse=True)  # 種の評価項目の平均との差の2乗が大きい順にソート
+
+  ic(score_list)
+
+  plant_list = [0]*60  # 良い順にソートされた種のリスト
+
+  for i in range(60):
+    plant_list[i] = score_list[i][0]
+
+  # ここからcosine類似度を使用してソート
+  top5_list = np.array([seed_list[i] for i in plant_list[:5]])  # 上位5個の種の評価項目リスト
+  ic(top5_list)
+
+  cos_sim_list = [[i, -1] for i in plant_list[:5]]  # cosine類似度リスト
+
+  for i in range(5):
+    for j in range(5):
+      cos_sim_list[i][1] += cos_sim(top5_list[i], top5_list[j])
+
+  ic(cos_sim_list)
+
+  cos_sim_list.sort(key=lambda x: x[1])  # cosine類似度が小さい順にソート
+
+  ic(cos_sim_list)
+
+  for i in range(5):
+    plant_list[i] = cos_sim_list[i][0]
+
+  ic(plant_list)
+
+  return plant_list
+
 # 種を適切な位置に植える
 def plant(seed_list):
   field = [[0] * n for _ in range(n)]  # 畑の初期化
@@ -123,7 +176,8 @@ def plant(seed_list):
   # plant_list = sort01(seed_list)  
   # plant_list = sort02(seed_list)
   # plant_list = sort03(seed_list)
-  plant_list = sort04(seed_list)
+  # plant_list = sort04(seed_list)
+  plant_list = sort05(seed_list)
   
   ic(plant_list)
 
@@ -144,6 +198,12 @@ def plant(seed_list):
   #               (0,2), (2,0), (2,4), (4,2), (0,1), (1,0), (0,3), (1,4), (3,0),
   #               (4,1), (3,4), (4,3), (0,0), (0,4), (4,0), (4,4), (2,5), (5,2),
   #               (1,5), (5,1), (3,5), (5,3), (0,5), (5,0), (4,5), (5,4), (5,5)]
+
+  # cosine類似度を使用する際の座標リスト
+  coord_list = [(2,2), (2,3), (3,2), (2,1), (1,2), (3,3), (1,3), (2,4), (3,1), 
+                (4,2), (3,4), (4,3), (1,1), (1,4), (4,1), (4,4), (0,2), (2,0), 
+                (0,3), (2,5), (3,0), (5,2), (3,5), (5,3), (0,1), (1,0), (0,4), 
+                (1,5), (4,0), (5,1), (4,5), (5,4), (0,0), (0,5), (5,0), (5,5)]
   
   # 種を畑に植える
   for i in range(36):
