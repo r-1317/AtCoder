8aa27bfc99435246656d50304151d3dd60ee63f2
r1317 <122949375+r-1317@users.noreply.github.com>
2025-09-15 10:24:27 +0900

    ABC423-Dまで

--- 2025/ABC423/d.py
+++ 2025/ABC423/d.py
@@ -1,5 +1,6 @@
 import os
 from collections import deque
+import heapq
 
 MyPC = os.path.basename(__file__) != "Main.py"
 if MyPC:
@@ -18,9 +19,25 @@ def main():
     a, b, c = map(int, input().split())
     dq.append((a, b, c))
 
-  t_list = []
+  ans_list = []
+  time = 0
   capacity = K
-  customer_queue = deque()
+  customer_queue = []
+  heapq.heapify(customer_queue)
+
+  while dq:
+    a, b, c = dq.popleft()
+    while capacity < c:
+      finish_time, finish_c = heapq.heappop(customer_queue)
+      time = finish_time
+      capacity += finish_c
+    time = max(time, a)
+    heapq.heappush(customer_queue, (time+b, c))
+    capacity -= c
+    ans_list.append(time)
+
+  for ans in ans_list:
+    print(ans)
 
 if __name__ == "__main__":
   main()
