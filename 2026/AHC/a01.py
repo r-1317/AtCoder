9fd230f2a13951080eecb2c56cad4ff68ece74c6
r1317 <122949375+r-1317@users.noreply.github.com>
2026-01-03 23:04:58 +0900

    テンプレートを2026にも用意

---
+++ 2026/AHC/a01.py
@@ -0,0 +1,18 @@
+import os
+from typing import Tuple, List
+
+MyPC = os.path.basename(__file__) != "Main.py"
+if MyPC:
+  from icecream import ic
+  ic.disable()
+else:
+  def ic(*args):
+    return None
+
+ic.enable() if MyPC else None
+
+def main():
+  pass
+
+if __name__ == "__main__":
+  main()
